b604589f471ee327d33aa2ee41f3d8a1ac372221
Naomi Lee <nlee16@cs.brown.edu>
2022-03-10 21:58:26 -0500

    pushing updates

--- compile.sh
+++ compile.sh
@@ -5,5 +5,4 @@
 ########################################
 
 # Update this file with instructions on how to compile your code
-pip3 install docplex
-# javac -classpath /local/projects/cplex/CPLEX_Studio_Academic/12.3/x86_64/cpoptimizer/lib/ILOG.CP.jar ./src/solver/cp/*.java
+javac -classpath /local/projects/cplex/CPLEX_Studio_Academic/12.3/x86_64/cpoptimizer/lib/ILOG.CP.jar ./src/solver/cp/*.java
---
+++ older/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+########################################
+############# CSCI 2951-O ##############
+########################################
+
+# Update this file with instructions on how to compile your code
+pip3 install docplex
+# javac -classpath /local/projects/cplex/CPLEX_Studio_Academic/12.3/x86_64/cpoptimizer/lib/ILOG.CP.jar ./src/solver/cp/*.java
